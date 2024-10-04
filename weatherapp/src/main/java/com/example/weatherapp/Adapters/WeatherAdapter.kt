fe153fd7da8bd6b8f2e55609d2dba8097f6d34d5
man7yct <101309883+abvgduyo8giu@users.noreply.github.com>
2024-10-04 17:01:31 +0900

    урок 10 закончен

--- weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherAdapter.kt
+++ weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherAdapter.kt
@@ -1,4 +1,18 @@
 package com.example.weatherapp.Adapters
 
-class WeatherAdapter {
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.weatherapp.databinding.ListItemBinding
+
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+    class Holder(view: View): RecyclerView.ViewHolder(view) {
+        val binding = ListItemBinding.bind(view)
+        fun bind(item: WeatherModel) = with(binding) {
+            tvDate.text = item.time
+            tvConditionList.text = item.condition
+            tvTemp.text = item.currentTemp
+        }
+    }
 }
