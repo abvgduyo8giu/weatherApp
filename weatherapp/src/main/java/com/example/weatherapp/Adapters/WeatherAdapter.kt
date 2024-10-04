b1b4068146636360fd93e839450cd3beea5bfc94
man7yct <101309883+abvgduyo8giu@users.noreply.github.com>
2024-10-04 17:20:21 +0900

    урок 11 закончен

--- weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherAdapter.kt
+++ weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherAdapter.kt
@@ -1,12 +1,16 @@
 package com.example.weatherapp.Adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.weatherapp.R
 import com.example.weatherapp.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
     class Holder(view: View): RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
         fun bind(item: WeatherModel) = with(binding) {
@@ -14,5 +18,25 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
             tvConditionList.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>() {
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(getItem(position))
     }
 }
--- weatherapp/src/main/java/com/example/weatherapp/fragments/HoursFragments.kt
+++ weatherapp/src/main/java/com/example/weatherapp/fragments/HoursFragments.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherapp.Adapters.WeatherAdapter
+import com.example.weatherapp.Adapters.WeatherModel
 import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentHoursBinding
 
 class HoursFragments : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding) {
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel("", "12:00", "sunny",
+                "25C", "",
+                "", "", ""),
+            WeatherModel("", "13:00", "sunny",
+                "26C", "",
+                "", "", ""),
+            WeatherModel("", "14:00", "sunny",
+                "-25C", "",
+                "", "", "")
+        )
+        adapter.submitList(list)
+    }
     companion object {
         @JvmStatic
         fun newInstance() = HoursFragments()
