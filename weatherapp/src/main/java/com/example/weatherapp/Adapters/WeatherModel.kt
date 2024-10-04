cc34cba77ba0750678f089d28a3ad44d38815e27
man7yct <101309883+abvgduyo8giu@users.noreply.github.com>
2024-10-04 16:42:25 +0900

    урок 8 закончен

--- weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherModel.kt
+++ weatherapp/src/main/java/com/example/weatherapp/Adapters/WeatherModel.kt
@@ -1,4 +1,12 @@
 package com.example.weatherapp.Adapters
 
-class WeatherModel {
-}
+data class WeatherModel(
+    val city: String,
+    val time: String,
+    val condition: String,
+    val currentTemp: String,
+    val maxTemp: String,
+    val minTemp: String,
+    val imgUrl: String,
+    val hours: String,
+)
